4f7d02f25cc71e298898016b8b499abe0ac21e52
Yaokun Pan <myron.yk.pan@gmail.com>
2018-09-03 13:51:49 +1000

    Finished task 3

--- src/app/task-two/task-two.component.ts
+++ src/app/task-two/task-two.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
@@ -6,6 +6,7 @@ import { Store } from '@ngrx/store';
 import * as fromRoot from '../reducers';
 import { ApiService } from '../api.service';
 import { Commit } from '../models/commit';
+import { MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
     selector: 'app-task-two',
@@ -13,26 +14,39 @@ import { Commit } from '../models/commit';
     styleUrls: ['./task-two.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TaskTwoComponent implements OnInit {
+export class TaskTwoComponent implements OnInit, OnDestroy {
 
     private actionsSubscription: Subscription;
 
-    public commits$: Observable<Commit[]>;
+    public dataSource: MatTableDataSource<Commit>;
 
     public displayedColumns: string[] = ['author', 'date', 'message'];
 
+    @ViewChild(MatSort) sort: MatSort;
+
     constructor( private store: Store<fromRoot.State>,
                  private apiService: ApiService,
-                 private route: ActivatedRoute ) {
+                 private route: ActivatedRoute,
+                 private cdRef: ChangeDetectorRef) {
     }
 
     ngOnInit() {
-        this.commits$ = this.route.params
+        this.actionsSubscription = this.route.params
             .pipe(
                 map(params => {
                     return {username: params.username, slug: params.slug};
                 }),
                 switchMap(( r ) => this.apiService.getCommits(r.username, r.slug))
-            );
+            ).subscribe((r) => {
+                if (r.length > 0) {
+                    this.dataSource = new MatTableDataSource(r);
+                    this.dataSource.sort = this.sort;
+                    this.cdRef.markForCheck();
+                }
+            });
+    }
+
+    public ngOnDestroy(): void {
+        this.actionsSubscription.unsubscribe();
     }
 }
